98020601183ddc9ba705f98174f672d9ff0ad0dd
Martin <marvahus@proton.me>
2023-12-01 16:10:23 +0100

    Initial

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+dir="$1"
+if [[ ! -d $dir ]]; then
+    echo "Directory '$dir' doesn't exist"
+    echo "Usage: ./build.sh <directory>"
+    echo "Example: ./build.sh 01"
+    exit 1
+fi
+
+path="$dir/main.jai"
+if [[ ! -f $path ]]; then
+    echo "Path '$path' doesn't exist"
+    echo "Make a main.jai file in the directory"
+    exit 1
+fi
+
+set -xe
+
+jai $path -quiet
+
+{ set +x; } &> /dev/null
+
+if [[ "$2" -eq "run" ]]; then
+    echo "=== RUNNING ==="
+    $dir/main
+fi
+
